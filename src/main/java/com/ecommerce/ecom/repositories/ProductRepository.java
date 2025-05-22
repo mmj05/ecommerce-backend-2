a55b33c96775c0769732568232fa9edce1c19144
muhaiminjobayer <mmj0993@gmail.com>
2025-05-22 13:05:41 -0500

    Add method to find products by user/seller in ProductRepository

--- src/main/java/com/ecommerce/ecom/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/ecom/repositories/ProductRepository.java
@@ -2,6 +2,7 @@ package com.ecommerce.ecom.repositories;
 
 import com.ecommerce.ecom.model.Category;
 import com.ecommerce.ecom.model.Product;
+import com.ecommerce.ecom.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Repository;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageable);
     Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageable);
+
+    // Find products by user/seller
+    Page<Product> findByUser(User user, Pageable pageable);
 
     boolean existsByProductName(String productName);
 }
