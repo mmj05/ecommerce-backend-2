ba8a6865b05276a12066e0897b20c6318572c30c
muhaiminjobayer <mmj0993@gmail.com>
2025-05-24 13:21:14 -0500

    Add method to OrderItemRepository for retrieving order items by product IDs

--- src/main/java/com/ecommerce/ecom/repositories/OrderItemRepository.java
+++ src/main/java/com/ecommerce/ecom/repositories/OrderItemRepository.java
@@ -4,6 +4,10 @@ import com.ecommerce.ecom.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    // Add this method for seller functionality
+    List<OrderItem> findByProductProductIdIn(List<Long> productIds);
 }
