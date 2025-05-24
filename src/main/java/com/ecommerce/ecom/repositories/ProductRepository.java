4a87dcd84ef41b9d7f200b2c5f73f5026ff1c62d
muhaiminjobayer <mmj0993@gmail.com>
2025-05-24 13:21:21 -0500

    Refactor imports in ProductRepository for cleaner code organization

--- src/main/java/com/ecommerce/ecom/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/ecom/repositories/ProductRepository.java
@@ -1,12 +1,12 @@
 package com.ecommerce.ecom.repositories;
 
-import com.ecommerce.ecom.model.Category;
-import com.ecommerce.ecom.model.Product;
-import com.ecommerce.ecom.model.User;
+import com.ecommerce.ecom.model.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -17,4 +17,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByUser(User user, Pageable pageable);
 
     boolean existsByProductName(String productName);
+
+    List<Product> findByUser(User user);
 }
